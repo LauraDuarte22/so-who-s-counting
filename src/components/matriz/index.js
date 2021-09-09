d103af603c7b5723670e20dc270463c32de66ec7
LauraDuarte22 <71296495+LauraDuarte22@users.noreply.github.com>
2021-09-08 22:27:29 -0500

    complete

--- src/components/matriz/index.js
+++ src/components/matriz/index.js
@@ -1,279 +1,104 @@
 import React from "react";
-import { Form, InputGroup, FormControl } from "react-bootstrap";
+import { Form } from "react-bootstrap";
 
 import "./index.css";
 
-export default class Matriz extends React.Component {
-  render() {
-    return (
-      <div>
-        <Form striped bordered hover>
-          <thead>
-            <tr>
-              <th>Número </th>
-              <th> 1</th>
-              <th> 2</th>
-              <th> 3</th>
-              <th> 4</th>
-              <th> 5</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td>0</td>
-              <td>
-                <InputGroup className="mb-3">
-                  <FormControl />
-                </InputGroup>
-              </td>
-              <td>
-                <InputGroup className="mb-3">
-                  <FormControl />
-                </InputGroup>
-              </td>
-              <td>
-                <InputGroup className="mb-3">
-                  <FormControl />
-                </InputGroup>
-              </td>
-              <td>
-                <InputGroup className="mb-3">
-                  <FormControl />
-                </InputGroup>
-              </td>
-              <td>
-                <InputGroup className="mb-3">
-                  <FormControl />
-                </InputGroup>
-              </td>
-            </tr>
-            <tr>
-              <td>1</td>
-              <td>
-                <InputGroup className="mb-3">
-                  <FormControl />
-                </InputGroup>
-              </td>
-              <td>
-                <InputGroup className="mb-3">
-                  <FormControl />
-                </InputGroup>
-              </td>
-              <td>
-                <InputGroup className="mb-3">
-                  <FormControl />
-                </InputGroup>
-              </td>
-              <td>
-                <InputGroup className="mb-3">
-                  <FormControl />
-                </InputGroup>
-              </td>
-              <td>
-                <InputGroup className="mb-3">
-                  <FormControl />
-                </InputGroup>
-              </td>
-            </tr>
-            <tr>
-              <td>2</td>
-              <td>
-                <InputGroup className="mb-3">
-                  <FormControl />
-                </InputGroup>
-              </td>
-              <td>
-                <InputGroup className="mb-3">
-                  <FormControl />
-                </InputGroup>
-              </td>
-              <td>
-                <InputGroup className="mb-3">
-                  <FormControl />
-                </InputGroup>
-              </td>
-              <td>
-                <InputGroup className="mb-3">
-                  <FormControl />
-                </InputGroup>
-              </td>
-              <td>
-                <InputGroup className="mb-3">
-                  <FormControl />
-                </InputGroup>
-              </td>
-            </tr>
-            <tr>
-              <td>3</td>
-              <td>
-                <InputGroup className="mb-3">
-                  <FormControl />
-                </InputGroup>
-              </td>
-              <td>
-                <InputGroup className="mb-3">
-                  <FormControl />
-                </InputGroup>
-              </td>
-              <td>
-                <InputGroup className="mb-3">
-                  <FormControl />
-                </InputGroup>
-              </td>
-              <td>
-                <InputGroup className="mb-3">
-                  <FormControl />
-                </InputGroup>
-              </td>
-              <td>
-                <InputGroup className="mb-3">
-                  <FormControl />
-                </InputGroup>
-              </td>
-            </tr>
-            <tr>
-              <td>4</td>
-              <td>
-                <InputGroup className="mb-3">
-                  <FormControl />
-                </InputGroup>
-              </td>
-              <td>
-                <InputGroup className="mb-3">
-                  <FormControl />
-                </InputGroup>
-              </td>
-              <td>
-                <InputGroup className="mb-3">
-                  <FormControl />
-                </InputGroup>
-              </td>
-              <td>
-                <InputGroup className="mb-3">
-                  <FormControl />
-                </InputGroup>
-              </td>
-              <td>
-                <InputGroup className="mb-3">
-                  <FormControl />
-                </InputGroup>
-              </td>
-            </tr>
-            <tr>
-              <td>5</td>
-              <td>
-                <InputGroup className="mb-3">
-                  <FormControl />
-                </InputGroup>
-              </td>
-              <td>
-                <InputGroup className="mb-3">
-                  <FormControl />
-                </InputGroup>
-              </td>
-              <td>
-                <InputGroup className="mb-3">
-                  <FormControl />
-                </InputGroup>
-              </td>
-              <td>
-                <InputGroup className="mb-3">
-                  <FormControl />
-                </InputGroup>
-              </td>
-              <td>
-                <InputGroup className="mb-3">
-                  <FormControl />
-                </InputGroup>
-              </td>
-            </tr>
-            <tr>
-              <td>6</td>
-              <td>
-                <InputGroup className="mb-3">
-                  <FormControl />
-                </InputGroup>
-              </td>
-              <td>
-                <InputGroup className="mb-3">
-                  <FormControl />
-                </InputGroup>
-              </td>
-              <td>
-                <InputGroup className="mb-3">
-                  <FormControl />
-                </InputGroup>
-              </td>
-              <td>
-                <InputGroup className="mb-3">
-                  <FormControl />
-                </InputGroup>
-              </td>
-              <td>
-                <InputGroup className="mb-3">
-                  <FormControl />
-                </InputGroup>
-              </td>
-            </tr>
-            <tr>
-              <td>7</td>
-              <td>
-                <InputGroup className="mb-3">
-                  <FormControl />
-                </InputGroup>
-              </td>
-              <td>
-                <InputGroup className="mb-3">
-                  <FormControl />
-                </InputGroup>
-              </td>
-              <td>
-                <InputGroup className="mb-3">
-                  <FormControl />
-                </InputGroup>
-              </td>
-              <td>
-                <InputGroup className="mb-3">
-                  <FormControl />
-                </InputGroup>
-              </td>
-              <td>
-                <InputGroup className="mb-3">
-                  <FormControl />
-                </InputGroup>
-              </td>
-            </tr>
-            <tr>
-              <td>9</td>
-              <td>
-                <InputGroup className="mb-3">
-                  <FormControl />
-                </InputGroup>
-              </td>{" "}
-              <td>
-                <InputGroup className="mb-3">
-                  <FormControl />
-                </InputGroup>
-              </td>{" "}
-              <td>
-                <InputGroup className="mb-3">
-                  <FormControl />
-                </InputGroup>
-              </td>{" "}
-              <td>
-                <InputGroup className="mb-3">
-                  <FormControl />
-                </InputGroup>
-              </td>{" "}
-              <td>
-                <InputGroup className="mb-3">
-                  <FormControl />
-                </InputGroup>
-              </td>
-            </tr>
-          </tbody>
-        </Form>
-      </div>
-    );
-  }
+export default function Matriz(props) {
+  const guardarDatos = async () => {
+    let bandera = true;
+    let cargoInfo = false;
+    let matrix = [
+      [1, 1, 1, 1, 1],
+      [1, 1, 1, 1, 1],
+      [1, 1, 1, 1, 1],
+      [1, 1, 1, 1, 1],
+      [1, 1, 1, 1, 1],
+      [1, 1, 1, 1, 1],
+      [1, 1, 1, 1, 1],
+      [1, 1, 1, 1, 1],
+      [1, 1, 1, 1, 1],
+      [1, 1, 1, 1, 1]
+    ];
+    for (let i = 0; i < props.estrategia.length; i++) {
+      for (let j = 0; j < props.estrategia[i].length; j++) {
+        let elemento = document.getElementById("elemento-" + i + "-" + j).value;
+        if (elemento !== undefined) {
+          matrix[i][j] = await elemento;
+        }
+        if (elemento === null || elemento === "" || elemento === 0) {
+          bandera = await false;
+        }
+        if (i === props.estrategia.length - 1) {
+          cargoInfo = await true;
+        }
+      }
+    }
+    if (bandera && cargoInfo) {
+      if (matrix !== undefined) {
+        props.setEstrategia(matrix);
+        alert("Estrategia guardada.");
+      }
+    } else {
+      alert("Faltan datos en la matriz.");
+    }
+  };
+
+  const renderMatriz = (value) => {
+    if (value) {
+      return (
+        <Matriz
+          estrategia={props.estrategia}
+          setEstrategia={props.setEstrategia}
+        />
+      );
+    } else {
+      return null;
+    }
+  };
+  return (
+    <div>
+      <Form striped bordered hover>
+        <thead>
+          <tr>
+            <th>Número </th>
+            <th> 1</th>
+            <th> 2</th>
+            <th> 3</th>
+            <th> 4</th>
+            <th> 5</th>
+          </tr>
+        </thead>
+        <tbody>
+          {props.estrategia.map((columns, indexCol) => {
+            return (
+              <tr>
+                <td>{indexCol}</td>
+                {columns.map((element, indexRow) => {
+                  return (
+                    <td>
+                      <input
+                        id={"elemento-" + indexCol + "-" + indexRow}
+                        style={{ width: 35 }}
+                        type="number"
+                        min="1"
+                        max="5"
+                      />
+                    </td>
+                  );
+                })}
+              </tr>
+            );
+          })}
+        </tbody>
+        <button
+          onClick={() => guardarDatos()}
+          type="button"
+          id="btn-flex"
+          className="btn btn-unique btn-sm"
+        >
+          guardar
+        </button>
+      </Form>
+    </div>
+  );
 }
